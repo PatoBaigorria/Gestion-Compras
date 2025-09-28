234087cb76d802b2daa61015016c0022ba34c0c4
patricia <mpbaigorria.01@gmail.com>
2025-09-28 05:23:09 -0300

    se agrega vista devoluciones, se agregausuarioa kardex y pedido

--- Models/Kardex.cs
+++ Models/Kardex.cs
@@ -4,14 +4,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Gestion_Compras.Models
 {
 
-	public class Kardex
-	{
-		[Key] 
-		public int Id { get; set; }
+    public class Kardex
+    {
+        [Key] 
+        public int Id { get; set; }
 
-		[ForeignKey(nameof(ItemId))]
-		public Item? Item { get; set; }
-		public int ItemId { get; set; }
+        [ForeignKey(nameof(ItemId))]
+        public Item? Item { get; set; }
+        public int ItemId { get; set; }
 
         public double StockIni { get; set; }
 
@@ -22,5 +22,10 @@ namespace Gestion_Compras.Models
         public DateTime FechaRegistro { get; set; }
 
         public DateOnly FechaMov { get; set; }
-	}
+
+        // Auditoría: usuario que realizó el movimiento
+        [ForeignKey(nameof(UsuarioId))]
+        public Usuario? Usuario { get; set; }
+        public int? UsuarioId { get; set; }
+    }
 }
